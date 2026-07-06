f04ac25355a774c7c1db6b14910a169fbacdcb9b
rndms <notrndms@gmail.com>
2026-07-06 20:16:26 +0800

    Update random_mod.user.js

--- random_mod.user.js
+++ random_mod.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Hordes.io edits + Random Mod
-// @version      3.4
+// @version      3.5
 // @author       Tuna & rndms
 // @description  Hordes.io custom client with Random Mod suite integrated
 // @match        https://hordes.io/play*
